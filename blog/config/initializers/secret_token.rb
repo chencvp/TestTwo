c99cc2dcd3476ca43425b9429c0e771ee27ff87f
Ubuntu <ubuntu@ip-172-31-5-70.ec2.internal>
2016-06-30 16:33:00 +0000

    Adding an entire subproject called "blog" (which was created using rails new blog) - this is from the Ubuntu EC2 instance.

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '4e9db5499f6baf452140e05f286321ff1bf85ae3caaafdfb6a0a76871d44d92e3d85a0b3b63e078716c097ebefcf536382d2413e300a2d7147dde8a2cc9dc5d3'
